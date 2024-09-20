344b451a14d01e0c1521e34336353d3fadce5843
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2024-09-19 21:33:11 -0400

    Almost final commit

--- src/com/coderscampus/UserLoginApplication.java
+++ src/com/coderscampus/UserLoginApplication.java
@@ -1,11 +1,23 @@
 package com.coderscampus;
 
+import java.util.Arrays;
+
 public class UserLoginApplication {
 
 	public static void main(String[] args) {
 		
+		UserService userService = new UserService();
 		
+		User[] user = userService.readFile(new String("C:\\Users\\15204\\OneDrive\\Documents\\"
+				+ "GitHub\\bootcamp_workspace\\yehoshua-assignment-3\\data.txt"));
+				
+//		for (User stuff : user) {
+//			System.out.println("Username: " + stuff.getUsername());
+//			System.out.println("Password: " + stuff.getPassword());
+//			System.out.println("Name: " + stuff.getName());
+//		}
 
-	}
+		System.out.println(userService.validateInput(user));
+	} 
 
 }
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -1,36 +1,75 @@
 package com.coderscampus;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class UserService {
 	
-	// reading data from file
+	// reading data from file ^
 	// creating User Array
 	// validating input
 	
 	public User[] readFile(String filePath) {
 		
 		BufferedReader fileReader = null;
-		fileReader = new BufferedReader(new FileReader(filePath));
-		User user = new User();
+		try {
+			fileReader = new BufferedReader(new FileReader(filePath));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		String line;
-		int counter = 0;
-		while ((line = fileReader.readLine()) != null) {
-			String[] stringArray = line.split(",");
-			user.setUsername(stringArray[0]);
-			user.setPassword(stringArray[1]);
-			user.setName(stringArray[2]);
-			counter++;
+		int index = 0;
+		User[] userArray = new User[4];;
+		try {
+			while ((line = fileReader.readLine()) != null) {
+				String[] stringArray = line.split(",");
+				User user = new User();
+				user.setUsername(stringArray[0]);
+				user.setPassword(stringArray[1]);
+				user.setName(stringArray[2]);
+				userArray[index] = user;
+				index++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
-		User[] userArray = new User[counter + 1];
-		return userArray;
-		
-		
-		fileReader.close();
-		
-		
-	}
+		try {
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			return userArray;
+		}
 	
+	public String validateInput(User[] userArray) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your email: ");
+		String emailInput = scanner.nextLine();
+		System.out.println("Enter your password: ");
+		String passwordInput = scanner.nextLine();
+		String output = "";
+		for (int i = 0; i < 6; i++) {		
+			for(int j = 0; j < userArray.length; j++) {
+				if (userArray[j].getUsername().equalsIgnoreCase(emailInput)  && 
+						userArray[j].getPassword().equalsIgnoreCase(passwordInput)) {
+					output = ("Welcome: " + userArray[j].getName());
+					break;
+				} else {
+					System.out.println("Invalid login, please try again");
+				}
+			}
+			if (output == "") {
+				output = "Too many failed login attempts, you are now locked out";
+			} else {
+				break ;
+			}
+		}
+		scanner.close();
+		return output;
+	}
 }
